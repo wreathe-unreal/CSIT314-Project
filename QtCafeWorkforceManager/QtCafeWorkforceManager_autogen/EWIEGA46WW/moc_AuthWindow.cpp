469cd32a369a60ebbaef2d9a61a59593c413840a
wreathe-unreal <110839486+wreathe-unreal@users.noreply.github.com>
2023-10-13 16:20:17 +0800

    Autogen Trash

--- QtCafeWorkforceManager/QtCafeWorkforceManager_autogen/EWIEGA46WW/moc_AuthWindow.cpp
+++ QtCafeWorkforceManager/QtCafeWorkforceManager_autogen/EWIEGA46WW/moc_AuthWindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'AuthWindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.3)
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -21,7 +21,7 @@ QT_BEGIN_MOC_NAMESPACE
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'AuthWindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 68
-#error "This file was generated using the moc from 6.5.3. It"
+#error "This file was generated using the moc from 6.6.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -41,25 +41,19 @@ static constexpr auto qt_meta_stringdata_CLASSAuthWindowENDCLASS = QtMocHelpers:
     "AuthWindow",
     "on_LoginButton_clicked",
     "",
-    "on_columnView_clicked",
-    "QModelIndex",
-    "index",
     "on_QLE_Username_textChanged",
     "arg1",
     "on_QLE_Password_textEdited"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSAuthWindowENDCLASS_t {
-    uint offsetsAndSizes[18];
+    uint offsetsAndSizes[12];
     char stringdata0[11];
     char stringdata1[23];
     char stringdata2[1];
-    char stringdata3[22];
-    char stringdata4[12];
-    char stringdata5[6];
-    char stringdata6[28];
-    char stringdata7[5];
-    char stringdata8[27];
+    char stringdata3[28];
+    char stringdata4[5];
+    char stringdata5[27];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSAuthWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -68,19 +62,13 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSAuthWindowENDCLASS_t qt_meta_st
         QT_MOC_LITERAL(0, 10),  // "AuthWindow"
         QT_MOC_LITERAL(11, 22),  // "on_LoginButton_clicked"
         QT_MOC_LITERAL(34, 0),  // ""
-        QT_MOC_LITERAL(35, 21),  // "on_columnView_clicked"
-        QT_MOC_LITERAL(57, 11),  // "QModelIndex"
-        QT_MOC_LITERAL(69, 5),  // "index"
-        QT_MOC_LITERAL(75, 27),  // "on_QLE_Username_textChanged"
-        QT_MOC_LITERAL(103, 4),  // "arg1"
-        QT_MOC_LITERAL(108, 26)   // "on_QLE_Password_textEdited"
+        QT_MOC_LITERAL(35, 27),  // "on_QLE_Username_textChanged"
+        QT_MOC_LITERAL(63, 4),  // "arg1"
+        QT_MOC_LITERAL(68, 26)   // "on_QLE_Password_textEdited"
     },
     "AuthWindow",
     "on_LoginButton_clicked",
     "",
-    "on_columnView_clicked",
-    "QModelIndex",
-    "index",
     "on_QLE_Username_textChanged",
     "arg1",
     "on_QLE_Password_textEdited"
@@ -92,10 +80,10 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSAuthWindowENDCLASS_t qt_meta_st
 Q_CONSTINIT static const uint qt_meta_data_CLASSAuthWindowENDCLASS[] = {
 
  // content:
-      11,       // revision
+      12,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -103,16 +91,14 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSAuthWindowENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   38,    2, 0x08,    1 /* Private */,
-       3,    1,   39,    2, 0x08,    2 /* Private */,
-       6,    1,   42,    2, 0x08,    4 /* Private */,
-       8,    1,   45,    2, 0x08,    6 /* Private */,
+       1,    0,   32,    2, 0x08,    1 /* Private */,
+       3,    1,   33,    2, 0x08,    2 /* Private */,
+       5,    1,   36,    2, 0x08,    4 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 4,    5,
-    QMetaType::Void, QMetaType::QString,    7,
-    QMetaType::Void, QMetaType::QString,    7,
+    QMetaType::Void, QMetaType::QString,    4,
+    QMetaType::Void, QMetaType::QString,    4,
 
        0        // eod
 };
@@ -128,9 +114,6 @@ Q_CONSTINIT const QMetaObject AuthWindow::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<AuthWindow, std::true_type>,
         // method 'on_LoginButton_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'on_columnView_clicked'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<const QModelIndex &, std::false_type>,
         // method 'on_QLE_Username_textChanged'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
@@ -148,9 +131,8 @@ void AuthWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         (void)_t;
         switch (_id) {
         case 0: _t->on_LoginButton_clicked(); break;
-        case 1: _t->on_columnView_clicked((*reinterpret_cast< std::add_pointer_t<QModelIndex>>(_a[1]))); break;
-        case 2: _t->on_QLE_Username_textChanged((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
-        case 3: _t->on_QLE_Password_textEdited((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 1: _t->on_QLE_Username_textChanged((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 2: _t->on_QLE_Password_textEdited((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
         default: ;
         }
     }
@@ -175,13 +157,13 @@ int AuthWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 3)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 4;
+        _id -= 3;
     }
     return _id;
 }
--- QtCafeWorkforceManager/QtCafeWorkforceManager_autogen/EWIEGA46WW/moc_mainwindow.cpp
+++ QtCafeWorkforceManager/QtCafeWorkforceManager_autogen/EWIEGA46WW/moc_mainwindow.cpp
@@ -1,12 +1,12 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'MainWindow.h'
+** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.3)
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../MainWindow.h"
+#include "../../mainwindow.h"
 #include <QtCore/qmetatype.h>
 
 #if __has_include(<QtCore/qtmochelpers.h>)
@@ -19,9 +19,9 @@ QT_BEGIN_MOC_NAMESPACE
 #include <memory>
 
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'MainWindow.h' doesn't include <QObject>."
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 68
-#error "This file was generated using the moc from 6.5.3. It"
+#error "This file was generated using the moc from 6.6.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -60,7 +60,7 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_st
 Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
 
  // content:
-      11,       // revision
+      12,       // revision
        0,       // classname
        0,    0, // classinfo
        0,    0, // methods
