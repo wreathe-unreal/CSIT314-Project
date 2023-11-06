1aea0f6f5bf152862baa3581fae7dd2604ef0eac
wreathe-unreal <elliot.n.andersen@gmail.com>
2023-11-06 20:37:12 +0800

    Fix Mistakes in last Commit

--- QtCafeWorkforceManager/main.cpp
+++ QtCafeWorkforceManager/main.cpp
@@ -9,6 +9,9 @@
 
 int main(int argc, char *argv[])
 {
+
+    qDebug() << QDir::currentPath();
+
     QApplicationGlobal a(argc, argv);
     QApplicationGlobal::setStyle(QStyleFactory::create("Fusion"));
 
@@ -16,12 +19,9 @@ int main(int argc, char *argv[])
     QString appDirPath = QApplication::applicationDirPath();
 
     // For the test database
-    QString testDbPath = QDir(appDirPath).filePath("../QtCafeWorkforceManager/TestDB.db");
-    QApplicationGlobal::CafeDB.setDatabaseName(testDbPath);
+    QString TestDBPath = QDir(appDirPath).filePath("../QtCafeWorkforceManager/TestDB.db");
+    QApplicationGlobal::CafeDB.setDatabaseName(TestDBPath);
 
-    // For the main database
-    QString mainDbPath = QDir(appDirPath).filePath("../QtCafeWorkforceManager/CafeDB.db");
-    QApplicationGlobal::CafeDB.setDatabaseName(mainDbPath);
 
     QApplicationGlobal::CafeDB.open();
     if (!QApplicationGlobal::CafeDB.open())
@@ -38,9 +38,14 @@ int main(int argc, char *argv[])
     QApplicationGlobal::CafeDB.close();
     //end unit tests
 
+
+
     //continue with program execution
     //change from testing TestDB.db to deployment CafeDB.db
-    QApplicationGlobal::CafeDB.open();
+
+    // For the main database
+    QString MainDBPath = QDir(appDirPath).filePath("../QtCafeWorkforceManager/CafeDB.db");
+    QApplicationGlobal::CafeDB.setDatabaseName(MainDBPath);
     if (!QApplicationGlobal::CafeDB.open())
     {
         qCritical() << "Failed to open main database";
